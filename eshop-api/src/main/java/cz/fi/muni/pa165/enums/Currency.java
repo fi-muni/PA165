9fa825bc75dea4d6367e48e2d7f6206fd39be091
Petr Adamek <petr.adamek@embedit.cz>
2015-09-29 11:40:12 +0200

    Second seminar branch

--- eshop-api/src/main/java/cz/fi/muni/pa165/enums/Currency.java
+++
@@ -1,5 +0,0 @@
-package cz.fi.muni.pa165.enums;
-
-public enum Currency {
-    CZK, EUR, USD
-}
--- eshop-persistence/src/main/java/cz/fi/muni/pa165/dao/PriceRepository.java
+++
@@ -1,9 +0,0 @@
-package cz.fi.muni.pa165.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import cz.fi.muni.pa165.entity.Order;
-import cz.fi.muni.pa165.entity.Price;
-
-public interface PriceRepository extends CrudRepository<Price, Long>  {
-}
