abdc48e604acccbe59e6c05326bd94cdf9a4eae2
Filip Nguyen <nguyen.filip@gmail.com>
2015-10-04 11:54:11 +0200

    Bean mapping service

---
+++ eshop-api/src/main/java/cz/fi/muni/pa165/dto/PriceDTO.java
@@ -0,0 +1,60 @@
+package cz.fi.muni.pa165.dto;
+
+import java.math.BigDecimal;
+
+import cz.fi.muni.pa165.enums.Currency;
+
+public class PriceDTO {
+	private Long id;
+	private BigDecimal value;
+	private Currency currency;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public BigDecimal getValue() {
+		return value;
+	}
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+	public Currency getCurrency() {
+		return currency;
+	}
+	public void setCurrency(Currency currency) {
+		this.currency = currency;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((currency == null) ? 0 : currency.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PriceDTO other = (PriceDTO) obj;
+		if (currency != other.currency)
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
+	
+	
+
+}
