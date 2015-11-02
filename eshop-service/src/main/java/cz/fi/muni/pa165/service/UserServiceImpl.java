6321a15281e85e57b6ce94601f994282c8759cbb
Martin Kuba <makub@ics.muni.cz>
2015-11-02 15:29:45 +0100

    replaced unsecure sha1 hash with secure pbkdf2 hash

--- eshop-service/src/main/java/cz/fi/muni/pa165/service/UserServiceImpl.java
+++ eshop-service/src/main/java/cz/fi/muni/pa165/service/UserServiceImpl.java
@@ -1,75 +1,113 @@
 package cz.fi.muni.pa165.service;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.List;
-
+import cz.fi.muni.pa165.dao.UserDao;
+import cz.fi.muni.pa165.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import cz.fi.muni.pa165.dao.UserDao;
-import cz.fi.muni.pa165.entity.User;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.List;
 
 /**
  * Implementation of the {@link UserService}. This class is part of the service module of the application that provides the implementation of the
  * business logic (main logic of the application).
  */
 @Service
-public class UserServiceImpl implements UserService
-{
-	@Autowired
+public class UserServiceImpl implements UserService {
+    @Autowired
     private UserDao userDao;
-	
+
     @Override
-    public void registerUser(User u, String unencryptedPassword)
-    {
-        u.setPasswordHash(makeSHA1Hash(unencryptedPassword));
+    public void registerUser(User u, String unencryptedPassword) {
+        u.setPasswordHash(createHash(unencryptedPassword));
         userDao.create(u);
     }
 
     @Override
-    public List<User> getAllUsers()
-    {
+    public List<User> getAllUsers() {
         return userDao.findAll();
     }
 
     @Override
-    public boolean authenticate(User u, String password)
-    {
-        return u.getPasswordHash().equals(makeSHA1Hash(password));
+    public boolean authenticate(User u, String password) {
+        return validatePassword(password, u.getPasswordHash());
     }
 
     @Override
-    public boolean isAdmin(User u)
-    {
+    public boolean isAdmin(User u) {
         return u.getGivenName().equals("admin");
     }
 
-    public String makeSHA1Hash(String input)
-    {
-        MessageDigest md = null;
-        try
-        {
-            md = MessageDigest.getInstance("SHA1");
-            md.reset();
-            byte[] buffer = input.getBytes();
-            md.update(buffer);
-            byte[] digest = md.digest();
+    @Override
+    public User findUserById(Long userId) {
+        return userDao.findById(userId);
+    }
 
-            String hexStr = "";
-            for (int i = 0; i < digest.length; i++)
-            {
-                hexStr += Integer.toString((digest[i] & 0xff) + 0x100, 16).substring(1);
-            }
-            return hexStr;
-        }
-        catch (NoSuchAlgorithmException e) {
+    //see  https://crackstation.net/hashing-security.htm#javasourcecode
+    private static String createHash(String password) {
+        final int SALT_BYTE_SIZE = 24;
+        final int HASH_BYTE_SIZE = 24;
+        final int PBKDF2_ITERATIONS = 1000;
+        // Generate a random salt
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[SALT_BYTE_SIZE];
+        random.nextBytes(salt);
+        // Hash the password
+        byte[] hash = pbkdf2(password.toCharArray(), salt, PBKDF2_ITERATIONS, HASH_BYTE_SIZE);
+        // format iterations:salt:hash
+        return PBKDF2_ITERATIONS + ":" + toHex(salt) + ":" + toHex(hash);
+    }
+
+    private static byte[] pbkdf2(char[] password, byte[] salt, int iterations, int bytes) {
+        try {
+            PBEKeySpec spec = new PBEKeySpec(password, salt, iterations, bytes * 8);
+            return SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256").generateSecret(spec).getEncoded();
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-	@Override
-	public User findUserById(Long userId) {
-		return userDao.findById(userId);
-	}
+    public static boolean validatePassword(String password, String correctHash) {
+        String[] params = correctHash.split(":");
+        int iterations = Integer.parseInt(params[0]);
+        byte[] salt = fromHex(params[1]);
+        byte[] hash = fromHex(params[2]);
+        byte[] testHash = pbkdf2(password.toCharArray(), salt, iterations, hash.length);
+        return slowEquals(hash, testHash);
+    }
+
+    /**
+     * Compares two byte arrays in length-constant time. This comparison method
+     * is used so that password hashes cannot be extracted from an on-line
+     * system using a timing attack and then attacked off-line.
+     *
+     * @param a the first byte array
+     * @param b the second byte array
+     * @return true if both byte arrays are the same, false if not
+     */
+    private static boolean slowEquals(byte[] a, byte[] b) {
+        int diff = a.length ^ b.length;
+        for (int i = 0; i < a.length && i < b.length; i++)
+            diff |= a[i] ^ b[i];
+        return diff == 0;
+    }
+
+    private static byte[] fromHex(String hex) {
+        byte[] binary = new byte[hex.length() / 2];
+        for (int i = 0; i < binary.length; i++) {
+            binary[i] = (byte) Integer.parseInt(hex.substring(2 * i, 2 * i + 2), 16);
+        }
+        return binary;
+    }
+
+    private static String toHex(byte[] array) {
+        BigInteger bi = new BigInteger(1, array);
+        String hex = bi.toString(16);
+        int paddingLength = (array.length * 2) - hex.length();
+        return paddingLength > 0 ? String.format("%0" + paddingLength + "d", 0) + hex : hex;
+    }
+
 }
