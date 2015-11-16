60811823aed60d097eb6ac41815563d0feb9b239
sakic <brossi@mail.muni.cz>
2015-11-16 14:19:20 +0100

    adding allow origin interceptor

--- eshop-rest/src/main/java/cz/fi/muni/pa165/RootWebContext.java
+++ eshop-rest/src/main/java/cz/fi/muni/pa165/RootWebContext.java
@@ -9,9 +9,20 @@ import cz.fi.muni.pa165.service.CategoryServiceImpl;
 import cz.fi.muni.pa165.service.config.ServiceConfiguration;
 import cz.fi.muni.pa165.service.facade.CategoryFacadeImpl;
 import cz.fi.muni.pa165.service.facade.OrderFacadeImpl;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 @EnableWebMvc
 @Configuration
 @Import({ServiceConfiguration.class,SpringMVCConfig.class})
-public class RootWebContext {
+public class RootWebContext extends WebMvcConfigurerAdapter {
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AllowOriginInterceptor()); 
+    }
+    
+    
+
+    
 }
