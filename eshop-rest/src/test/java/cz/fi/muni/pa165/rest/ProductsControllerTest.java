17c496fbb5e4982127bcec6d276a7fc50d286260
sakic <brossi@mail.muni.cz>
2015-11-21 22:33:08 +0100

    step4 seminar10
    (cherry picked from commit 97adfeaca4f8690495452e94e532918c23acb5ac)

--- eshop-rest/src/test/java/cz/fi/muni/pa165/rest/ProductsControllerTest.java
+++ eshop-rest/src/test/java/cz/fi/muni/pa165/rest/ProductsControllerTest.java
@@ -1,9 +1,16 @@
 package cz.fi.muni.pa165.rest;
 
 import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
@@ -17,6 +24,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
@@ -31,6 +39,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import cz.fi.muni.pa165.RootWebContext;
 import cz.fi.muni.pa165.dto.CategoryDTO;
 import cz.fi.muni.pa165.dto.Color;
+import cz.fi.muni.pa165.dto.NewPriceDTO;
 import cz.fi.muni.pa165.dto.PriceDTO;
 import cz.fi.muni.pa165.dto.ProductCreateDTO;
 import cz.fi.muni.pa165.dto.ProductDTO;
@@ -61,16 +70,6 @@ public class ProductsControllerTest extends AbstractTestNGSpringContextTests {
         mockMvc = standaloneSetup(productsController).setMessageConverters(new MappingJackson2HttpMessageConverter()).build();
     }
 
-    /**
-     * This is not a real test, but it is here to show the usage of mockMvc to
-     * perform a GET request and also how both request and response can be
-     * printed out
-     *
-     * TODO: you can run this test and see the information that is outputted
-     *
-     *
-     *
-     */
     @Test
     public void debugTest() throws Exception {
         doReturn(Collections.unmodifiableList(this.createProducts())).when(
@@ -78,52 +77,74 @@ public class ProductsControllerTest extends AbstractTestNGSpringContextTests {
         mockMvc.perform(get("/products")).andDo(print());
     }
 
-    /**
-     * TODO: in this test we want to ensure the following: 1. the status of the
-     * response is OK 200 2. the content type is
-     * MediaType.APPLICATION_JSON_VALUE 3. we check that the name for two
-     * products with ids 10 and 20 are Raspberry PI and Arduino (see preloaded
-     * data in createProducts()
-     *
-     * The first point is already implemented, we can chain further andExpect()
-     * for 2nd and 3rd point
-     *
-     * To do this, you can use
-     * org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get()
-     * org.springframework.test.web.servlet.result.MockMvcResultMatchers.content()
-     * org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath()
-     * org.springframework.test.web.servlet.result.MockMvcResultMatchers.status()
-     *
-     * You can read about Jsonpath notation from the documentation
-     * https://github.com/jayway/JsonPath to define the expression in jsonPath()
-     * method so that you will have similar statement as
-     * JsonPath("<expression>").value("Raspberry PI")
-     *
-     * From the previous debug method, what we are expected to parse is
-     * [{"id":10,"image":null,"imageMimeType":null,"name":"Raspberry PI",
-     *
-     */
     @Test
     public void getAllProducts() throws Exception {
 
         doReturn(Collections.unmodifiableList(this.createProducts())).when(
                 productFacade).getAllProducts();
 
-        mockMvc.perform(get(ApiUris.ROOT_URI_PRODUCTS))
+        mockMvc.perform(get("/products"))
+                .andExpect(status().isOk())
+                .andExpect(
+                        content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(
+                        jsonPath("$.[?(@.id==10)].name").value("Raspberry PI"))
+                .andExpect(jsonPath("$.[?(@.id==20)].name").value("Arduino"));
+
+    }
+
+    @Test
+    public void getValidProduct() throws Exception {
+
+        List<ProductDTO> products = this.createProducts();
+
+        doReturn(products.get(0)).when(productFacade).getProductWithId(10l);
+        doReturn(products.get(1)).when(productFacade).getProductWithId(20l);
+
+        mockMvc.perform(get("/products/10"))
+                .andExpect(status().isOk())
+                .andExpect(
+                        content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.name").value("Raspberry PI"));
+        mockMvc.perform(get("/products/20"))
+                .andExpect(status().isOk())
+                .andExpect(
+                        content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.name").value("Arduino"));
+
+    }
+
+    @Test
+    public void getInvalidProduct() throws Exception {
+        doThrow(new RuntimeException()).when(productFacade).getProductWithId(1l);
+
+        mockMvc.perform(get("/products/1")).andExpect(
+                status().is4xxClientError());
+
+    }
+
+    @Test
+    public void deleteProduct() throws Exception {
+
+        List<ProductDTO> products = this.createProducts();
+
+        mockMvc.perform(delete("/products/10"))
                 .andExpect(status().isOk());
 
     }
 
-    /**
-     * TODO: In this method we are testing the creation of one product use
-     * mockMvc to:
-     *
-     * 1. perform a POST 2. Set the content type to APPLICATION_JSON 3. convert
-     * the ProductCreateDTO instance to JSON with the helper method
-     * convertObjectToJsonBytes() and pass it with content() in mockMvc
-     * perform() 4. test also that the status is 200 OK
-     *
-     */
+    @Test
+    public void deleteProductNonExisting() throws Exception {
+
+        List<ProductDTO> products = this.createProducts();
+
+        doThrow(new RuntimeException("the product does not exist")).when(productFacade).deleteProduct(20l);
+
+        mockMvc.perform(delete("/products/20"))
+                .andExpect(status().isNotFound());
+
+    }
+
     @Test
     public void createProduct() throws Exception {
 
@@ -135,16 +156,33 @@ public class ProductsControllerTest extends AbstractTestNGSpringContextTests {
 
         String json = this.convertObjectToJsonBytes(productCreateDTO);
 
+        System.out.println(json);
+
+        mockMvc.perform(
+                post("/products/create").contentType(MediaType.APPLICATION_JSON)
+                .content(json)).andDo(print())
+                .andExpect(status().isOk());
     }
 
-    /**
-     * TODO: Test the POST to products for the addition of a category the
-     * mapping is at "/products/{prod_id}/categories" Use again the json
-     * content, set the content type, and expect to get OK 200
-     *
-     *
-     * @throws Exception
-     */
+    @Test
+    public void updateProduct() throws Exception {
+        List<ProductDTO> products = this.createProducts();
+
+        doReturn(products.get(0)).when(productFacade).getProductWithId(10l);
+        doReturn(products.get(1)).when(productFacade).getProductWithId(20l);
+
+        doNothing().when(productFacade).changePrice(any(NewPriceDTO.class));
+        NewPriceDTO newPrice = new NewPriceDTO();
+
+        String json = this.convertObjectToJsonBytes(newPrice);
+
+        mockMvc.perform(
+                put("/products/10").contentType(MediaType.APPLICATION_JSON)
+                .content(json)).andDo(print())
+                .andExpect(status().isOk());
+
+    }
+
     @Test
     public void addCategory() throws Exception {
         List<ProductDTO> products = this.createProducts();
@@ -156,28 +194,23 @@ public class ProductsControllerTest extends AbstractTestNGSpringContextTests {
         category.setId(1l);
 
         String json = this.convertObjectToJsonBytes(category);
+
+        mockMvc.perform(
+                post("/products/10/categories").contentType(
+                        MediaType.APPLICATION_JSON).content(json))
+                .andDo(print()).andExpect(status().isOk());
+
     }
 
-    /**
-     * TODO: 
-     * 
-     * Let's test for a non-existing product
-     * 
-     * 1. use mockito to throw an exception when a specific productid is not available
-     * (Note: we would return here null but see comment in the RestController
-     * implementation)
-     * 2. test that the return code is 404 or in 4xx HTTP range
-     * 
-     * 
-     */
     @Test
     public void getNonExistingProduct() throws Exception {
+        doThrow(new RuntimeException()).when(productFacade).getProductWithId(1l);
+
+        mockMvc.perform(get("/products/1"))
+                .andExpect(status().is4xxClientError());
 
     }
 
-    /**
-     * Just an utility method to create products for testing
-     */
     private List<ProductDTO> createProducts() {
         ProductDTO productOne = new ProductDTO();
         productOne.setId(10L);
@@ -200,9 +233,6 @@ public class ProductsControllerTest extends AbstractTestNGSpringContextTests {
         return Arrays.asList(productOne, productTwo);
     }
 
-    /**
-     * Just an utility method to convert Objects to bytes to check JSON format
-     */
     private static String convertObjectToJsonBytes(Object object)
             throws IOException {
         ObjectMapper mapper = new ObjectMapper();
