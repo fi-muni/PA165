816664bee0364fb44e97ab44ce8b2d77fecf4863
Martin Kuba <makub@ics.muni.cz>
2015-12-04 13:38:57 +0100

    seminar-javascript complete solution

--- eshop-angular/src/main/webapp/angular_app.js
+++ eshop-angular/src/main/webapp/angular_app.js
@@ -14,6 +14,11 @@ pa165eshopApp.config(['$routeProvider',
         when('/category/:categoryId', {templateUrl: 'partials/category_detail.html', controller: 'CategoryDetailCtrl'}).
         when('/admin/products', {templateUrl: 'partials/admin_products.html', controller: 'AdminProductsCtrl'}).
         when('/admin/newproduct', {templateUrl: 'partials/admin_new_product.html', controller: 'AdminNewProductCtrl'}).
+        when('/admin/categories', {templateUrl: 'partials/admin_categories.html', controller: 'AdminCategoriesCtrl'}).
+        when('/admin/newcategory', {
+            templateUrl: 'partials/admin_new_category.html',
+            controller: 'AdminNewCategoryCtrl'
+        }).
         otherwise({redirectTo: '/shopping'});
     }]);
 
@@ -120,7 +125,7 @@ eshopControllers.controller('AdminProductsCtrl',
         loadAdminProducts($http, $scope);
         // function called when Delete button is clicked
         $scope.deleteProduct = function (product) {
-	    console.log("deleting product with id=" + product.id + ' (' + product.name + ')');
+            console.log("deleting product with id=" + product.id + ' (' + product.name + ')');
             $http.delete(product._links.delete.href).then(
                 function success(response) {
                     console.log('deleted product ' + product.id + ' on server');
@@ -212,3 +217,54 @@ eshopControllers.directive('convertToInt', function () {
         }
     };
 });
+
+/*
+ * Admin categories
+ */
+eshopControllers.controller('AdminCategoriesCtrl',
+    function ($scope, $rootScope, $routeParams, $http) {
+        //initial load of all categories
+        $http.get('/eshop/api/v1/categories').then(function (response) {
+            $scope.categories = response.data['_embedded']['categoryDTOList'];
+        });
+    });
+
+/*
+ * Page with form for creating new category
+ */
+eshopControllers.controller('AdminNewCategoryCtrl',
+    function ($scope, $routeParams, $http, $location, $rootScope) {
+        //set object bound to form fields
+        $scope.category = {
+            'name': ''
+        };
+        // function called when submit button is clicked, creates category on server
+        $scope.create = function (category) {
+            $http({
+                method: 'POST',
+                url: '/eshop/api/v1/categories/create',
+                data: category
+            }).then(function success(response) {
+                var createdCategory = response.data;
+                //display confirmation alert
+                $rootScope.successAlert = 'A new category "' + createdCategory.name + '" was created';
+                //change view to list of products
+                $location.path("/admin/categories");
+            }, function error(response) {
+                //display error
+                console.log("error when creating category");
+                console.log(response);
+                switch (response.data.code) {
+                    case 'PersistenceException':
+                        $rootScope.errorAlert = 'Category with the same name already exists ! ';
+                        break;
+                    case 'InvalidRequestException':
+                        $rootScope.errorAlert = 'Sent data were found to be invalid by server ! ';
+                        break;
+                    default:
+                        $rootScope.errorAlert = 'Cannot create category ! Reason given by the server: '+response.data.message;
+                        break;
+                }
+            });
+        };
+    });
