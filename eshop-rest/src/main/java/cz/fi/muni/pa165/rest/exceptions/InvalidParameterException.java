915236ac1bde6f6b2e0b229d905329e79ee39fb3
Martin Kuba <makub@ics.muni.cz>
2015-12-04 11:41:00 +0100

    seminar-javascript step 0

--- eshop-rest/src/main/java/cz/fi/muni/pa165/rest/exceptions/InvalidParameterException.java
+++
@@ -1,9 +0,0 @@
-package cz.fi.muni.pa165.rest.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE)
-public class InvalidParameterException extends RuntimeException {
-    
-} 
