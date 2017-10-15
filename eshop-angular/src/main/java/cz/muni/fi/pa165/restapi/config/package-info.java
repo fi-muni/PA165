e11ee79c09e2420eb6f75a494308eb29b21184d3
Uhlir <jiri.uhlir@solarwinds.com>
2017-10-15 18:15:31 +0200

    Cleanup other projects for seminar branch

--- eshop-angular/src/main/java/cz/muni/fi/pa165/restapi/config/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package contains classes which configure SpringMVC and Spring context.
- *
- * @author Martin Kuba makub@ics.muni.cz
- */
-package cz.muni.fi.pa165.restapi.config;
--- eshop-api/src/main/java/cz/fi/muni/pa165/dto/Color.java
+++
@@ -1,5 +0,0 @@
-package cz.fi.muni.pa165.dto;
-
-public enum Color {
-   BLACK, WHITE, RED, GREEN, BLUE, ORANGE, YELLOW, AZURE, MAGENTA, BROWN, PINK, GRAY, UNDEFINED
-}
